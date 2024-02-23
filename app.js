a5a5a846d2e8156383ea451d97fdab1941d44b94
khalidlotfi <khalidlotfi2005@gmail.com>
2024-02-23 19:25:58 +0100

    connect mongodb ..
    Signed-off-by: khalidlotfi <khalidlotfi2005@gmail.com>

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const url = process.env.MONGODB_URI;
+
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/about", (req, res) => res.send("About us"));
 
-app.listen(3000, () => {
-  console.log("listening on Port 3000 ...");
-});
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("connected to Mongodb");
+    app.listen(3000, () => console.log("listening on server 3000...."));
+  })
+  .catch((err) => console.log(err));
