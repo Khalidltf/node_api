0a7562b82a052a27258e5d941ab00f1c1b23777d
khalidlotfi <khalidlotfi2005@gmail.com>
2024-02-26 23:20:19 +0100

    Delete Data in MongoDB

--- app.js
+++ app.js
@@ -45,10 +45,22 @@ app.put("/products/:id", async (req, res) => {
     res.status(500).json({ message: error.json });
   }
 });
-
 app.post("/product", async (req, res) => {
   try {
     const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) res.json(404).json({ message: `no product with id ${id}` });
+
     res.status(200).json(product);
   } catch (error) {
     console.log(error.message);
