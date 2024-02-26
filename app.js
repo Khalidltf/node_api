618bf4a8d90b0432d0c9154da68ef4634d34b119
khalidlotfi <khalidlotfi2005@gmail.com>
2024-02-26 19:32:34 +0100

    fetch data from MongoDB

--- app.js
+++ app.js
@@ -9,6 +9,28 @@ const Product = require("./models/productModel");
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.get("/about", (req, res) => res.send("About us"));
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
 app.post("/product", async (req, res) => {
   try {
     const product = await Product.create(req.body);
